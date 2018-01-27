f17f32dab3563f53b60d3c16d984314b9f739132
yeongtaek, Hong <mozodev@users.noreply.github.com>
2018-01-27 19:41:29 +0900

    초기 화면 제목을 추가하고 배너는 하나만 유지합니다.

--- src/settings.php
+++ src/settings.php
@@ -3,7 +3,6 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-
         'view' => [
             'template_path' => __DIR__ . '/../templates',
             'twig' => [
@@ -12,7 +11,6 @@ return [
                 'auto_reload' => true,
             ]
         ],
-
         // Monolog settings
         'logger' => [
             'name' => 'slim-app',
