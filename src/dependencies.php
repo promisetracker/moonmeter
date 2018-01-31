29a918b475fc44bd5e2a26a33ca56d60c7755e97
yeongtaek, Hong <mozodev@users.noreply.github.com>
2018-02-01 07:54:31 +0900

    디비 연결하고 초기 화면에 진행 중인 공약과 위계 구조를 출력해봅니다.

--- src/dependencies.php
+++ src/dependencies.php
@@ -29,3 +29,14 @@ $container['logger'] = function ($c) {
     return $logger;
 };
 
+// Service factory for the ORM
+$container['db'] = function ($c) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($c['settings']['db']);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
+
--- src/routes.php
+++ src/routes.php
@@ -5,9 +5,33 @@ use Slim\Http\Response;
 use Slim\Views\Twig;
 
 // Routes
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
 	$name = !empty($args['name']) ? $args['name'] : '';
 	$this->logger->notice($name);
     // Render index view
+    $visions = $this->db->table('p_4vision')->get();
+    $promises = $this->db->table('p_12promise')->get();
+    $categories = $this->db->table('p_12promise_category')->get();
+    $test = $promises->groupBY('pv_no');
+    $activePromises = $this->db->table('sub_promise')->where('promise_level', '<>', '0')->orderBy('promise_level', 'desc')->get();
+
+    $status = [
+    	"평가안됨(준비안됨)",
+    	"시작안함",
+    	"진행중",
+    	"변경(공약)",
+    	"지체(정체)",
+    	"파기",
+    	"공약 이행(완료)",
+    ];
+    
+    $this->logger->debug($activePromises);
+    $args += [
+    	'visions' => $visions,
+    	'promises' => $promises,
+    	'categories' => $categories,
+    	'activePromises' => $activePromises,
+    	'status' => $status
+    ];
     return $this->view->render($response, 'index.twig', $args);
 });
